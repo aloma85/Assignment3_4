446e602ce980a6dee68c3680652133d372945338
aloma85 <idrisun22@gmail.com>
2021-03-14 23:11:55 -0500

    11:11

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ dir = os.path.abspath(os.path.dirname(__file__))
 login_manager.init_app(app)
 login_manager.login_view = "login"
 admin = Admin(app)
+
+@login_manager.user_loader
+def load_user(userid):
+    return Users.query.get(userid)
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    note = relationship("Notes", backref="Users", lazy=True)
